d5b7799677d7786b406c7f06a807475e57532a74
lycos44 <wolfgang.auer@gmx.net>
2025-08-09 21:21:27 +0200

    again

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # raspi-client
+# https://medium.com/@taraszhere/coding-remote-procedure-call-rpc-with-python-3b14a7d00ac8
+# https://github.com/TarasZhere/RPC
--- src/client.py
+++ src/client.py
@@ -1,11 +1,12 @@
 from rpc import RPCClient
 
 #server = RPCClient('192.168.178.43', 8080)
-server = RPCClient('10.42.0.3', 8080)
+#server = RPCClient('10.42.0.3', 8080)
+server = RPCClient('192.168.178.35', 8080)
 
 server.connect()
 
-print(server.add(5, -16))
+print(server.add(5, 16))
 print(server.sub(5, 6))
 
 server.disconnect()
