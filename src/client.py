bb250a64d895d6221472239ad926fd668892106f
lycos44 <wolfgang.auer@gmx.net>
2025-10-27 14:30:10 +0100

    implemented client.py and status.py files to interact with the server and monitor status. Updated .gitignore to exclude server.txt file.

--- src/client.py
+++ src/client.py
@@ -1,29 +1,48 @@
+from collections import namedtuple
 import logging
-import logging.config
-import os
+import sys
+import argparse
+import json
 from rpc import RPCClient
+from status import Status
+from logging.handlers import TimedRotatingFileHandler
+from time import sleep
 
 logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(module)s : %(lineno)d - %(message)s',
-                    handlers=[logging.FileHandler("logs/raspi-client.log"),
+                    format='%(asctime)s - %(module)s - %(levelname)s - %(funcName)s : %(lineno)d - %(message)s',
+                    handlers=[TimedRotatingFileHandler("logs/raspi-client.log", when="midnight", interval=1, backupCount=7),
                               logging.StreamHandler()])
 
 # create logger
 logger = logging.getLogger('raspi-client')
 
 def main():
-    #server = RPCClient('192.168.178.43', 8080)
-    server = RPCClient('192.168.178.35', 8080)
+    parser = argparse.ArgumentParser(description="Raspi Client.")
+    parser.add_argument('--address', type=str, help='EV3 Server IPv4 address.')
+    parser.add_argument('--port', type=int, help='EV3 Server port.')
 
-    server.connect()
-    logger.info('Connected to server at %s', server._RPCClient__address)
+    args = parser.parse_args()
+    if args.address and args.port:
+        server = RPCClient(args.address, args.port)
+    else:
+        exit("Please provide both --address and --port arguments.")
+        
+    try:
+        server.connect()
+        logger.info('Connected to server at %s', server._RPCClient__address)
+        
+        logger.info('Request result: {}'.format(server.drive_forward()))
+        sleep(2)
+        logger.info('Request result: {}'.format(server.stop()))
 
-    logger.info('Request result: {}'.format(server.add(5, 16)))
-    logger.info('Request result: {}'.format(server.add(5, 6)))
-
-    server.disconnect()
-    logger.info('Disconnected from server')
-
+        logger.info('Request result: {}'.format(server.drive_backward()))
+        sleep(2)
+        logger.info('Request result: {}'.format(server.stop()))
+    except Exception as e:
+        logger.error('An error occurred: %s', e)
+    finally:
+        server.disconnect()
+        logger.info('Disconnected from server')
 
 if __name__ == '__main__':
     main() 
---
+++ src/status.py
@@ -0,0 +1,14 @@
+import json
+import logging
+
+logger = logging.getLogger(__name__)
+
+class Status:
+    def __init__(self, x):
+        self.datetime = x.datetime
+        self.direction = x.direction
+        self.speed = x.speed
+        self.looking = x.looking
+        self.distance = x.distance
+        self.color = x.color
+        self.angle = x.angle
